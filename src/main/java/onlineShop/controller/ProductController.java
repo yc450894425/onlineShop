7a4a6d77cced3e8ac4d8473c9e3a545b0b1ab75b
yuchaozhang <zhangyuchao0104@gmail.com>
2021-01-23 21:00:45 -0500

    Cart, Product Implementation.

--- src/main/java/onlineShop/controller/ProductController.java
+++ src/main/java/onlineShop/controller/ProductController.java
@@ -4,10 +4,19 @@ import onlineShop.model.Product;
 import onlineShop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Controller
@@ -31,6 +40,57 @@ public class ProductController {
     @RequestMapping(value = "/admin/product/addProduct", method = RequestMethod.GET)
     public ModelAndView getProductForm() {
         return new ModelAndView("addProduct", "productFormm", new Product());
+    }
+
+    @RequestMapping(value = "/admin/product/addProduct", method = RequestMethod.POST)
+    public String addProduct(@ModelAttribute(value = "productForm") Product product, BindingResult result) {
+        if (result.hasErrors()) {
+            return "addProduct";
+        }
+        productService.addProduct(product);
+        MultipartFile image = product.getProductImage();
+        if (image != null && !image.isEmpty()) {
+
+            Path path = Paths.get("/users/zhangyuchao/products/onlineShopping/onlineShopping/source/main/webapp/WEB-INF/resource/images/products/" + product.getId() + ".jpg");
+
+            try {
+                image.transferTo(new File(path.toString()));
+            } catch (IllegalStateException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return "redirect:/getAllProducts";
+    }
+
+    @RequestMapping(value = "admin/delete/{productId}")
+    public String deleteProduct(@PathVariable(value = "productId") int productId) {
+        Path path = Paths.get("/users/zhangyuchao/products/onlineShopping/onlineShopping/source/main/webapp/WEB-INF/resource/images/products/" + productId + ".jpg");
+        if (Files.exists(path)) {
+            try {
+                Files.delete(path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        productService.deleteProduct(productId);
+        return "redirect:/getAllProducts";
+    }
+
+    @RequestMapping(value = "admin/product/editProduct/{productId}", method = RequestMethod.GET)
+    public ModelAndView getEditForm(@PathVariable(value = "productId") int productId) {
+        Product product = productService.getProductById(productId);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("editProduct");
+        modelAndView.addObject("editProductObj", product);
+        modelAndView.addObject("productId", productId);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "admin/product/editProduct/{productId}", method = RequestMethod.POST)
+    public String editProduct(@ModelAttribute(value = "editProductObj") Product product, @PathVariable(value = "productId") int productId) {
+        product.setId(productId);
+        productService.updateProduct(product);
+        return "redirect:/getAllProducts";
     }
 
 }
